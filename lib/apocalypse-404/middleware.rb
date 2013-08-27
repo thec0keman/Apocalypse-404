3a1927e9cfbd46e7f203f06a49132740a240d1e8
runthral <john.w.ratcliff@biola.edu>
2013-08-26 23:26:52 -0700

    Fix dir

--- lib/apocalypse-404/middleware.rb
+++ lib/apocalypse-404/middleware.rb
@@ -12,8 +12,14 @@ class Rack::Apocalypse404
       puts " -- #{status} -- Redirecting to apocalyptic message...."
 
       spec = Gem::Specification.find_by_name("apocalypse-404")
-      filename = File.join( spec.gem_dir, 'app', 'html' ) + "/404.html"
-      response = [File.read( filename ).gsub!(/!!MESSAGE/, status.to_s )]
+      filename = File.join( spec.gem_dir, 'lib', 'html' ) + "/404.html"
+      unless File.readable(filename)
+        puts "HTML file was not found at #{filename}"
+        puts "Files in that directory are: " + Dir.foreach( File.join(t, 'html') ).inject("") { |res, v| res += "#{v}\n"}
+        response = "Error"
+      else
+        response = [File.read( filename ).gsub!(/!!MESSAGE/, status.to_s )]
+      end
 
       headers = { "Content-Length" => response[0].length.to_s, 
                   "Content-Type" => 'text/html;charset=utf-8' }
@@ -22,3 +28,7 @@ class Rack::Apocalypse404
     [ status, headers, response ]
   end
 end
+
+#t = ["#{File.dirname(File.expand_path($0))}/../lib/#{Apocalypse404::NAME}",
+#     "#{Gem.dir}/gems/#{Apocalypse404::NAME}-#{Apocalypse404::VERSION}/lib/#{Apocalypse404::NAME}"]
+#f = t.each {|i| return i if File.readable?(i) }
