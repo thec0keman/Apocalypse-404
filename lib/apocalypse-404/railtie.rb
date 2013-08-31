9f36faec77a7b81059ac84f5c499cf15c8115d65
runthral <john.w.ratcliff@biola.edu>
2013-08-31 13:48:04 -0700

    Removed require

--- lib/apocalypse-404/railtie.rb
+++ lib/apocalypse-404/railtie.rb
@@ -1,7 +1,6 @@
 module Apocalypse404
   class Railtie < Rails::Railtie
     initializer '404apocalypse.insert_middleware' do |app|
-      require "404-apocalypse/middleware"
       app.config.middleware.use Rack::Apocalypse404
     end
   end
