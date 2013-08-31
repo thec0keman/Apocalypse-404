4fc8e8692915dc8c25acd2d81bafaa4b31bdfdad
runthral <john.w.ratcliff@biola.edu>
2013-08-31 13:30:09 -0700

    Fixed railtie reference

--- lib/apocalypse-404/railtie.rb
+++ lib/apocalypse-404/railtie.rb
@@ -1,7 +1,7 @@
 module Apocalypse404
   class Railtie < Rails::Railtie
     initializer '404apocalypse.insert_middleware' do |app|
-      require "404-apocalypse/middleware"
+      require "middleware"
       app.config.middleware.use Rack::Apocalypse404
     end
   end
